f6b154508d16b1e15c0e84e7190e78761587f31f
eleele <2369745961@qq.com>
2019-02-10 17:11:36 +0800

    Added some features

--- a.js
+++ a.js
@@ -6,7 +6,22 @@ function range(a,b,step=1){
 function randint(a,b){
 	return Math.floor(Math.random()*(b-a)+a+1e-6)
 }
+function tick(){
+	if (vm.gs!=1) return
+	++vm.tm
+	setTimeout(tick,1000)
+}
+function check(){
+	for (var i=0; i<vm.n*vm.n-1; ++i)
+		if (vm.a[i]!=i+1) return false
+	return true
+}
 function move(i,j){
+	if (vm.gs==2) return
+	if (vm.gs==0){
+		setTimeout(tick,1000)
+		vm.gs=1
+	}
 	var di=[-1,1,0,0],dj=[0,0,-1,1]
 	for (var w=0; w<4; ++w){
 		var ni=i+di[w],nj=j+dj[w]
@@ -15,16 +30,29 @@ function move(i,j){
 			if (vm.a[y]==0){
 				Vue.set(vm.a,y,vm.a[x])
 				Vue.set(vm.a,x,0)
+				++vm.stp
+				if (check()){
+					vm.gs=2
+				}
 				return
 			}
 		}
 	}
 }
-function initGame(){
-	vm.a=range(1,vm.n*vm.n).concat(0)
-	for (var p=0; p<100*vm.n*vm.n; ++p){
-		move(randint(0,vm.n),randint(0,vm.n))
+function initGame(flag){
+	if (!flag){
+		vm.a=range(1,vm.n*vm.n).concat(0)
+		for (var p=0; p<100*vm.n*vm.n; ++p){
+			move(randint(0,vm.n),randint(0,vm.n))
+		}
+		vm.b=Array.from(vm.a)
 	}
+	else{
+		vm.a=Array.from(vm.b)
+	}
+	vm.gs=0
+	vm.stp=0
+	vm.tm=0
 }
 function backup(flag){
 	if (!flag && document.visibilityState!='hidden') return
